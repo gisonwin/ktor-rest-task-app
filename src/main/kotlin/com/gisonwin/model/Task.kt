2532700d74a613d4b8dbf4a227373f47655f9113
Gison Win <wenjx@linose.com>
2024-12-19 14:36:37 +0800

    modify

--- src/main/kotlin/com/gisonwin/model/Task.kt
+++ src/main/kotlin/com/gisonwin/model/Task.kt
@@ -1,4 +1,10 @@
 package com.gisonwin.model
 
-class Task {
+import kotlinx.serialization.Serializable
+
+enum class Priority {
+    Low, Medium, High, Vital
 }
+
+@Serializable
+data class Task(val name: String, val description: String, val priority: Priority)
